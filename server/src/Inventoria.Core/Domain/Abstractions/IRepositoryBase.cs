c86e25d7b09b373df79b64744535a2e70a76c779
Guilherme <guibyn@gmail.com>
2025-09-13 13:02:56 -0300

    chore: starts unitofwork

--- server/src/Inventoria.Core/Domain/Abstractions/IRepositoryBase.cs
+++ server/src/Inventoria.Core/Domain/Abstractions/IRepositoryBase.cs
@@ -1,10 +1,13 @@
-﻿namespace Inventoria.Core.Domain.Abstractions;
+﻿using System.Data;
+
+namespace Inventoria.Core.Domain.Abstractions;
 
 public interface IRepositoryBase<T>
 {
     Task<T?> GetByIdAsync(int id);
     Task<IEnumerable<T>> GetAllAsync();
-    Task AddAsync(T entity);
-    Task UpdateAsync(T entity);
-    Task DeleteAsync(T entity);
+    Task<bool> ExistsAsync(int id);
+    Task AddAsync(T entity, IUnitOfWork unitOfWork);
+    Task UpdateAsync(T entity, IUnitOfWork unitOfWork);
+    Task DeleteAsync(T entity, IUnitOfWork unitOfWork);
 }
---
+++ server/src/Inventoria.Core/Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventoria.Core.Domain.Abstractions;
+
+public interface IUnitOfWork : IDisposable
+{
+    public void BeginTransaction();
+    public void Commit();
+    public void Rollback();
+
+}
