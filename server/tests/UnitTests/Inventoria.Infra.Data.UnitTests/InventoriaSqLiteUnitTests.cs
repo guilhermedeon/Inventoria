355266481f233b99f0a95037fabd0448897d6693
Guilherme <guibyn@gmail.com>
2025-09-19 22:19:46 -0300

    feat: Adds unittests

---
+++ server/tests/UnitTests/Inventoria.Infra.Data.UnitTests/InventoriaSqLiteUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+using Xunit.Sdk;
+
+namespace Inventoria.Infra.Data.UnitTests;
+
+public class InventoriaSqLiteUnitTests
+{
+    [Fact]
+    public void CreateDbConnection_ReturnsSqliteConnection()
+    {
+        using var db = new InventoriaSqLite();
+        using var conn = db.CreateDbConnection();
+        Assert.IsType<SqliteConnection>(conn);
+    }
+
+    [Fact]
+    public void GetDbConnection_OpensConnection()
+    {
+        using var db = new InventoriaSqLite();
+        using var conn = db.GetDbConnection();
+        Assert.Equal(ConnectionState.Open, conn.State);
+    }
+
+    [Fact]
+    public void ReturnDbConnection_ClosedConnection_Disposes()
+    {
+        using var db = new InventoriaSqLite();
+        var conn = db.CreateDbConnection();
+        conn.Open();
+        conn.Close();
+
+        db.ReturnDbConnection(conn);
+
+        Assert.Equal(ConnectionState.Closed, conn.State);
+    }
+}
