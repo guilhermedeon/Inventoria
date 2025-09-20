4167b226ce150824291c58ba2b03bff7c3f7e5a0
Guilherme <guibyn@gmail.com>
2025-09-20 17:09:06 -0300

    feat: inventory create

---
+++ server/src/Inventoria.Core/Application/Features/Inventories/Create/InventoryCreateCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventoria.Core.Application.Features.Inventories.Create;
+
+public record InventoryCreateCommand
+    (
+        int UserId,
+        string Name,
+        DateTime CreatedAt,
+        string? Description
+    );
---
+++ server/src/Inventoria.Core/Application/Features/Inventories/Create/InventoryCreateCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Inventoria.Core.Domain.Entities.Inventories;
+using Inventoria.Core.Domain.Entities.Inventories.Abstractions;
+
+namespace Inventoria.Core.Application.Features.Inventories.Create;
+
+public class InventoryCreateCommandHandler(IInventoryRepository inventoryRepository)
+{
+    public async Task HandleAsync(InventoryCreateCommand command)
+    {
+        var inventory = new Inventory
+        {
+            UserId = command.UserId,
+            Name = command.Name,
+            CreatedAt = command.CreatedAt,
+            Description = command.Description
+        };
+
+        await inventoryRepository.AddAsync(inventory);
+    }
+}
+
